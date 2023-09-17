f2bee2436386a1f570a59d2cea689c583f0f5bfa
mcgoobie <43033770+mcgoobie@users.noreply.github.com>
2023-09-17 18:22:05 +0800

    Delete chat directory

--- chat/routing.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path , include
-from chat.consumer import *
- 
-# Route to the URL ChatConsumer (ASGI : Async Server Getway Interface)
-websocket_urlpatterns = [
-    path("" , ChatConsumer.as_asgi()) ,
-]
